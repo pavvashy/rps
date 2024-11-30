54e333d7d1dbf20b2b99d2f100aea373714892b0
Julinna <julinnaea@gmail.com>
2024-11-29 19:59:16 -0800

    update getHumanChoice function from if/else to switch statement

--- rps.js
+++ rps.js
@@ -1,4 +1,9 @@
-let computerChoice= 1
+// score
+let humanScore = 0
+let computerScore = 0
+
+for (let i = 0; i < 3; i++) {
+let computerChoice = Math.ceil(Math.random() * 3);
 
 function getComputerChoice() {
 switch (computerChoice) {
@@ -12,29 +17,32 @@ switch (computerChoice) {
         return null;
     }
 }
+
+//** finalComputerChoice variable is made to translate the switch statement
+// computerChoice inside the getComputerChoice function to a variable. 
+// same logic applies to finalHumanChoice **
 let finalComputerChoice = getComputerChoice(computerChoice)
 console.log("Bot chose: " + finalComputerChoice)
 
 let humanChoice = prompt("Choose rock, paper, or scissors")
 
 function getHumanChoice(humanChoice) {
-        if (humanChoice.toLowerCase() == "rock") {
-            return "rock"
-        }
-        else if (humanChoice.toLowerCase() == "paper") {
-            return "paper"
-        }
-        else (humanChoice.toLowerCase() == "scissors" || "scissor") 
-            return "scissors"
-        
+    switch (humanChoice) {
+        case humanChoice.toLowerCase()=="rock": 
+            return "rock";
+        case humanChoice.toLowerCase()=="paper":
+            return "paper";
+        case humanChoice.toLowerCase()=="scissors":
+            return "scissors";
+        default:
+            return null;
+    }
 }
-let finalHumanChoice = getHumanChoice(humanChoice)
 
+// finalHumanChoice translates entire function getHumanChoice to a variable
+let finalHumanChoice = getHumanChoice(humanChoice)
 console.log("You chose: " + finalHumanChoice)
 
-// score
-let humanScore = 0
-let computerScore = 0
 
 // play round function
 function playRound(humanChoice, computerChoice) {
@@ -85,7 +93,19 @@ function playRound(humanChoice, computerChoice) {
         }
 }
 }
-// consoles
 console.log(playRound(finalHumanChoice, finalComputerChoice))
+}
+
+// consoles
+
 console.log(computerScore)
 console.log(humanScore)
+
+if (computerScore < humanScore) {
+    console.log("YOU WIN!!!!!!!!!")
+}
+else if (computerScore = humanScore) {
+    console.log("Tie..............")
+}
+else if (computerScore > humanScore) 
+    console.log("YOU LOSE LOL")
