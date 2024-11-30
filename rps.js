8caf697b127426741bc1ffce51816380e9bcdf58
Julinna <julinnaea@gmail.com>
2024-11-29 18:19:18 -0800

    Add finalComputerChoice and finalHumanChoice
    Add both let values to translate getComputerChoice and getHumanChoice functions to variables. Also add playRound function (just about complete).

--- rps.js
+++ rps.js
@@ -1,4 +1,4 @@
-let computerChoice= Math.ceil(Math.random() * 3);
+let computerChoice= 1
 
 function getComputerChoice() {
 switch (computerChoice) {
@@ -12,7 +12,8 @@ switch (computerChoice) {
         return null;
     }
 }
-console.log("Bot chose: " + getComputerChoice(computerChoice))
+let finalComputerChoice = getComputerChoice(computerChoice)
+console.log("Bot chose: " + finalComputerChoice)
 
 let humanChoice = prompt("Choose rock, paper, or scissors")
 
@@ -23,7 +24,68 @@ function getHumanChoice(humanChoice) {
         else if (humanChoice.toLowerCase() == "paper") {
             return "paper"
         }
-        else (humanChoice.toLowerCase() == "scissors") 
+        else (humanChoice.toLowerCase() == "scissors" || "scissor") 
             return "scissors"
+        
 }
-console.log(getHumanChoice(humanChoice))
+let finalHumanChoice = getHumanChoice(humanChoice)
+
+console.log("You chose: " + finalHumanChoice)
+
+// score
+let humanScore = 0
+let computerScore = 0
+
+// play round function
+function playRound(humanChoice, computerChoice) {
+    // bot chose rock
+    if(computerChoice == "rock") {
+        if(humanChoice == "rock") {
+            return "Tie! Both chose " + computerChoice;
+        }
+        else if(humanChoice == "scissors") {
+            computerScore++
+            return "You lose!" + computerChoice + " beats " + humanChoice;
+    
+        }
+        else {
+            humanScore++
+            return "You win!" + humanChoice + " beats " + computerChoice
+        
+        }
+    }
+
+    // bot chose paper
+    if(computerChoice == "paper") {
+        if(humanChoice == "rock") {
+            computerScore++
+            return "You lose!" + computerChoice + " beats " + humanChoice; 
+        }
+        else if(humanChoice == "scissors") {
+            humanScore++
+            return "You win! " + humanChoice + " beats" + computerChoice;
+        }
+        else {
+            return "Tie! Both chose " + computerChoice;
+        }
+    }
+// bot chose scissors
+    if(computerChoice == "scissors") {
+        if(humanChoice == "rock") {
+            humanScore++
+            return "You win! " + humanChoice + " beats" + computerChoice;
+        }
+        else if(humanChoice == "scissors") {
+            humanScore++
+            return "Tie! Both chose " + computerChoice;
+        }
+        else {
+            computerScore++
+            return "You lose!" + computerChoice + " beats " + humanChoice; 
+        }
+}
+}
+// consoles
+console.log(playRound(finalHumanChoice, finalComputerChoice))
+console.log(computerScore)
+console.log(humanScore)
